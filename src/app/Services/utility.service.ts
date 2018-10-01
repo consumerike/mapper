c2fb0cafcbf19202c3bbf55deab806f290007884
Nwaelele Ikechukwu <EJZ7102@hca.corpad.net>
2018-10-01 15:44:18 -0500

    Added modification of user name for planview post

--- src/app/Services/utility.service.ts
+++ src/app/Services/utility.service.ts
@@ -13,4 +13,22 @@ export class UtilityService {
     console.log('Does this have the requisite two slashes::', modifiedUserName);
     return modifiedUserName;
   }
+
+  
+  modifyCurrentUser(currentUser:string): string {
+    let modifiedUserName = currentUser.toLowerCase();
+    let strippedUserID = modifiedUserName.substring(modifiedUserName.indexOf("\\")+1);
+    console.log('checking stripped user name',strippedUserID);
+    return strippedUserID;
+  }
+
+  modifyUrlSegmentForEncoding(currentUser: string): string {
+    //Note: there is also a encordeURIComponent() function that is for different special character encoding...
+    let urlSegment = `${currentUser}`
+    let encodedSegment = encodeURI(urlSegment);
+    console.log("the encoded segment within utility function",encodedSegment);
+    
+    return encodedSegment;
+  }
 }
+
